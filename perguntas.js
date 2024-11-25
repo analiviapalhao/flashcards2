805938f443dad593d09cc9dfdc079d3184d5077b
analiviapalhao <168450115+analiviapalhao@users.noreply.github.com>
2024-11-25 09:20:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Castelo animado',
+    'quem é o bruxo do filme?',
+    'O Howl'
+)
+
+criaCartao(
+    'Castelo animado',
+    'qual o lugar que os personagens moram no filme?',
+    'em um castelo'
+)
+
+criaCartao(
+    'Castelo animado',
+    'Porque a Sophie se transforma em idosa e jovem?',
+    'Por conta da maldição da bruxa'
+)
+
+criaCartao(
+    'Castelo animado',
+    'Quando foi lançado o filme?',
+    'em 15 de julho de 2005'
+)
